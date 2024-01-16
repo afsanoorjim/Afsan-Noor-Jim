9ace3be88bfd8c9af116394727873cd31337e438
Afsan Noor Jim <150720347+afsanoorjim@users.noreply.github.com>
2024-01-16 18:17:36 +0600

    Delete HI directory

--- HI/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import MonthlyFinancialData
-
-class FinancialDataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MonthlyFinancialData
-        fields = '__all__'
--- HI/urls.py
+++
@@ -1,9 +0,0 @@
-# financial_app/urls.py
-
-from django.urls import path
-from .views import FinancialDataListCreateView, FinancialData
-
-urlpatterns = [
-    path('financial_analysis/', FinancialData , name= 'FinancialData'),
-    path('api/financial-data/', FinancialDataListCreateView.as_view(), name='financial_data_api'),
-]
